bfda915922d4e2701997a16d6c9a7aa149faf0f1
Calin <the8thnoteofficial@gmail.com>
2019-01-18 09:30:52 +0000

    project setup complete

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO FILM (id, title, genre, description) VALUES (1, 'The Lion King', 'Horror', 'The terrifying tale of death, despair and lions and shit.');
+INSERT INTO FILM (id, title, genre, description) VALUES (2, 'The Room', 'Shit', 'Oh hi Mark');
+INSERT INTO FILM (id, title, genre, description) VALUES (3, 'The Human Centipede', 'Family', 'Feed Her!');
